e4f4be9c8af957e6c4e21a1cf6c1a971c202e12b
Danya <danya.desimus@gmail.com>
2022-08-08 20:20:01 +0300

    Hello

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from task1 import House
 
 h1 = House()
 
-h1.konder(3)
-h1.konder(3)
-h1.konder(3)
-h1.konder(3)
+h1.konder(2)
+h1.konder(5)
+h1.konder(6)
+h1.konder(8)
 
-h1.batareya(10)
+h1.batareya(17)
+
+
+print("Hello world")
